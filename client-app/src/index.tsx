a40edc71a833db12a432ff34a8d41ef72eefd208
bosko.kojic <bosko.kojic@LT-BKOJIC>
2023-05-17 15:43:37 +0200

    End of section 7

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -4,6 +4,7 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { StoreContext, store } from './app/stores/store';
 
 
 const root = ReactDOM.createRoot(
@@ -11,7 +12,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   //  <React.StrictMode>
+  <StoreContext.Provider value={store}>
     <App />
+  </StoreContext.Provider>
+    
   //  </React.StrictMode>
 );
 
