f2904615abfd79da222021c73c06e3c8f75acb4d
bosko.kojic <bosko.kojic@LT-BKOJIC>
2023-09-14 15:11:07 +0200

    Refresh Token Complete

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -33,7 +33,7 @@ axios.interceptors.response.use(async response => {
     return response;
 
 }, (error: AxiosError) => {
-    const { data, status, config } = error.response as AxiosResponse;
+    const { data, status, config, headers } = error.response as AxiosResponse;
     switch (status) {
         case 400:
             //toast.error('bad request');
@@ -54,7 +54,10 @@ axios.interceptors.response.use(async response => {
             }
             break;
         case 401:
-            toast.error('unauthorised');
+            if (status === 401 && headers['www-authenticate']?.startsWith('Bearer error="invalid_token"')) {
+                store.userStore.logout();
+                toast.error('Session expired - please login again');
+            }
             break;
         case 403:
             toast.error('forbidden');
@@ -95,6 +98,7 @@ const Account = {
     current: () => requests.get<User>('/account'),
     login: (user: UserFormValues) => requests.post<User>('/account/login', user),
     register: (user: UserFormValues) => requests.post<User>('/account/register', user),
+    refreshToken: () => requests.post<User>('/account/refreshToken', {})
 }
 
 const Profiles = {
